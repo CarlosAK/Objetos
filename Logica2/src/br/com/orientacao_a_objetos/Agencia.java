413a5b2ecc93d05375fea0d04248b6bed6646267
carlos <carlos.koser8@gmail.com>
2019-06-25 20:33:58 -0300

    Respostas 1 ao 41

---
+++ Logica2/src/br/com/orientacao_a_objetos/Agencia.java
@@ -0,0 +1,14 @@
+package br.com.orientacao_a_objetos;
+
+public class Agencia {
+	
+	public int agencia1;
+	public int agencia2;
+	public int numero;
+	
+	Agencia(int numero){
+		this.numero = numero;
+	}
+	
+
+}
---
+++ Logica2/src/br/com/orientacao_a_objetos/CartaoCredito.java
@@ -0,0 +1,15 @@
+package br.com.orientacao_a_objetos;
+
+public class CartaoCredito {
+	
+	public int NmCartao;
+	public String Validade;
+	public Clientes cliente;
+	public int numero;
+	
+	CartaoCredito(int numero){
+		this.numero = numero;
+	}
+	
+
+}
